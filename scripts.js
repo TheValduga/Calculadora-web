ba9a9522873f924cb81c4a2040c1b7821b1917db
TheValduga <lukinhas.valduga@gmail.com>
2022-09-09 11:55:53 -0300

    aa

--- scripts.js
+++ scripts.js
@@ -1,20 +1,55 @@
-const previousDisplay = document.getElementById("previous")
-const currentDisplay = document.getElementById("current")
-// const buttons = {...document.getElementsByTagName("button")}
-const buttons = document.querySelectorAll("#buttons_container button")
-console.log(buttons);
+const previousDisplay = document.getElementById("previous");
+const currentDisplay = document.getElementById("current");
+const buttons = document.querySelectorAll("#buttons_container button");
 
 class calculator {
+   constructor(previousDisplay,currentDisplay) {
+      this.previousDisplay = previousDisplay;
+      this.currentDisplay = currentDisplay;
+      this.currentOperation = '';
+   }
 
+   addDigit(digit) {
+      if(digit === ',' && this.currentDisplay.innerText.includes(',')) {
+         return;
+      }
+      this.currentOperation = digit;
+      this.updateScreen();
+   }
+
+   processOperation(operation) {
+      let operationValue;
+      const previous = +this.previousDisplay.innerText;
+      const current = +this.currentDisplay.innerText;
+
+      switch(operation) {
+         case '+':
+            operationValue = previous + current;
+            this.updateScreen
+      }
+   }
+
+   updateScreen(
+      operationsValue = null, 
+      operation = null, 
+      current = null, 
+      previous = null
+      ) {
+   this.currentDisplay.innerText += this.currentOperation;
+   }
 }
+
+const calc = new calculator(previousDisplay,currentDisplay)
 
 buttons.forEach((btn) => {
    btn.addEventListener("click",(event) => {
-   const value = event.target.innerText;
-   console.log(value);
-   /*if (+value >= 0 || value === ',') {
-
-   }*/
+      const value = event.target.innerText;
+      
+      if (+value >= 0 || value === ',') {
+         calc.addDigit(value);
+      } else {
+         calc.processOperation(value);
+      }
    }) 
    
 })
